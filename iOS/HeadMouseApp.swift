ae67b07027067ce504e565290e9ab1cb8495d768
fyp6jyt8y5-collab <fyp6jyt8y5-collab@users.noreply.github.com>
2026-08-26 12:47:25 +0200

    Launch Head Mouse as separate app

---
+++ iOS/HeadMouseApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct HeadMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                HeadMouseView()
+            }
+        }
+    }
+}
